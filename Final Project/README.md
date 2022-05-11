37dff3587ae62fa988ebe20d38d2cad5a96d3f66
Carrie Sundra <casundra@users.noreply.github.com>
2022-05-10 19:08:04 -0700

    Added Final Project based off Console and Blinky with Debug

---
+++ Final Project/README.md
@@ -0,0 +1,10 @@
+# The palLED
+## Making Embedded Systems Final Project
+
+Details:
+* RP2040 on an Rpi Pico dev board
+* Using VSCode with Platformio
+* Platform is Wizio, baremetal (using Rpi Pico SDK)
+* Debug tool is a Picoprobe
+
+
---
+++ Final Project/src/main.c
@@ -0,0 +1,56 @@
+/*************************************************
+** Command Line Interface
+** This is from https://github.com/eleciawhite/reusable
+** (MIT license)
+**
+** To use it in the simulator, press run 
+**    It should print `Welcome to the Consolinator, your gateway to testing code and hardware`
+** Type 'help' in the white box at the bottom and hit return.
+** It lists the commands it supports.
+**
+** There are several files here:
+** Console.c is a command parser. 
+**    Call the init function during init and
+**    then call the process function on every pass through a loop.
+** You probably don't need to modify this file much.
+**
+** The interface is in consoleIo.c which currently
+** uses the RPi Pico interface. You will need to modify this
+** for your own hardware, connecting it to a UART in your embedded system.
+**
+** The command table and commands are implemented in consoleCommands.c.
+** This is where you add commands, probably by copying one that is 
+** already there. You need to modify mConsoleCommandTable 
+** to add the command.
+*************************************************/
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "console.h"
+
+#define LED_PIN	25
+#define BLINK_TIME	1000000	// microseconds
+
+int main() {
+	stdio_init_all(); // UART setup for both input and output
+	gpio_init(LED_PIN);
+	gpio_set_dir(LED_PIN, GPIO_OUT);
+	sleep_ms(2000);
+	ConsoleInit();
+
+	absolute_time_t lastBlink = 0;
+	absolute_time_t lastWrite = 0;
+
+	while(1) 
+	{
+		// does not work
+		ConsoleProcess();
+
+		if ( get_absolute_time() - lastBlink > BLINK_TIME ) {
+			gpio_xor_mask(1 << LED_PIN);
+			lastBlink = get_absolute_time();
+            printf("Hello World");
+		}
+
+    	sleep_ms(2); 
+	}	
+}
